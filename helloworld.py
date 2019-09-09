53be56df58ad6ce1227d62b850a69428dff9b622
Kelsey Toporski <ktops@umich.edu>
2019-09-09 13:31:44 -0400

    initial commit

---
+++ helloworld.py
@@ -0,0 +1,18 @@
+# Using python, make your script print:
+
+# Your name
+print('Kelsey Toporski')
+
+# Two random numbers between 0-100
+import random
+oop = []
+for i in range(2):
+    a = random.randint(0, 101)
+    oop.append(a)
+    print(str(a))
+
+# The sum of the two numbers, starting with "Sum = "
+print('Sum = ' + str(sum(oop)))
+
+# The average of the two numbers, starting with "Average = "
+print('Average = ' + str((sum(oop)/len(oop))))
